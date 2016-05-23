5869abcef7cb24bf51113e133c89cf889ac402b4
sherwin m <sherwinm@gmail.com>
2016-05-23 16:09:12 -0700

    added index doc added edit()

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -10,6 +10,10 @@ use App\Models\Client;
 
 class ClientController extends Controller
 {
+    /**
+     * Display all client records.
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
     public function index()
     {
         $data = [
@@ -20,5 +24,17 @@ class ClientController extends Controller
         ];
 
         return view('clients.index', $data);
+    }
+
+    public function edit($id)
+    {
+        $data = [
+            'page_title'        => 'Edit Client',
+            'navi_group'        => 'clients',
+            'navi_submenu'      => 'edit',
+            'client'           => Client::find($id)
+        ];
+
+        return view('clients.edit', $data);
     }
 }
