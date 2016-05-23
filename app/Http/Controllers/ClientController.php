533e541f7f30c2f0be071916c2b3ed1af51ff33c
sherwin m <sherwinm@gmail.com>
2016-05-23 16:34:26 -0700

    add update()

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -42,4 +42,19 @@ class ClientController extends Controller
 
         return view('clients.edit', $data);
     }
+
+    /**
+     * Update client record.
+     * @param Requests\ClientRequest $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function update(Requests\ClientRequest $request)
+    {
+        if (Client::updateRecord($request))
+        {
+            return back()->with('success', 'Client updated.');
+        }
+
+        return back()->withInput()->with('error', 'Client failed to be updated.');
+    }
 }
