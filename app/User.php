a5c1ad9c9e81d27448105355d91edd5e521e21f5
sherwin m <sherwinm@gmail.com>
2016-05-23 16:04:08 -0700

    added hasRoles() and relationshiop to role

--- app/User.php
+++ app/User.php
@@ -4,6 +4,8 @@ namespace App;
 
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
+use Auth;
+
 class User extends Authenticatable
 {
     /**
@@ -23,4 +25,36 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    /**
+     * Relationship to role.
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function role()
+    {
+        return $this->belongsTo('App\Models\Role');
+    }
+
+    /**
+     * Check the role of authenticated user.
+     * @param $role_names
+     * @return bool
+     */
+    public static function hasRoles($role_names)
+    {
+        if (Auth::check())
+        {
+            $role_names = explode('|', $role_names);
+            // check the authenticated user's role against the database
+            $check_role = User::select('roles.role_name')
+                ->join('roles', 'roles.id', '=', 'users.role_id')
+                ->where('users.id', Auth::User()->id)
+                ->whereIn('roles.role_name', $role_names)
+                ->first();
+
+            return $check_role;
+        }
+
+        return false;
+    }
 }
