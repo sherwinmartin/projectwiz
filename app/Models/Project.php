22991c1bcbd9690283b8c84183df1b0282b82dbb
sherwin m <sherwinm@gmail.com>
2016-05-24 10:32:00 -0700

    initial commit

---
+++ app/Models/Project.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Project extends Model
+{
+    protected $table = 'projects';
+
+    public function client()
+    {
+        return $this->belongsTo('App\Models\Client');
+    }
+}
