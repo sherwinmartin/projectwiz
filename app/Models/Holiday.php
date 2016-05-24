09408c5322c27e6e7cc3ae925e37aa5b07172daa
sherwin m <sherwinm@gmail.com>
2016-05-24 12:16:51 -0700

    added crud methods

--- app/Models/Holiday.php
+++ app/Models/Holiday.php
@@ -6,5 +6,43 @@ use Illuminate\Database\Eloquent\Model;
 
 class Holiday extends Model
 {
-    //
+    protected $table = 'holidays';
+
+    protected $fillable = [
+        'holiday_name', 'holiday_date'
+    ];
+
+    /**
+     * Create new holiday record.
+     * @param $request
+     * @return bool
+     */
+    public static function storeRecord($request)
+    {
+        $holiday = new Holiday;
+
+        if ($holiday::create($request->all()))
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Update holiday record.
+     * @param $request
+     * @return bool
+     */
+    public static function updateRecord($request)
+    {
+        $holiday = Holiday::find($request['id']);
+
+        if ($holiday->update($request->all()))
+        {
+            return true;
+        }
+
+        return false;
+    }
 }
