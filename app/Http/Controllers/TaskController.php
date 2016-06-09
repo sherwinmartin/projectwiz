1273c43adbf0b06b4bf6cf7b84435d36dde4d40f
sherwin m <sherwinm@gmail.com>
2016-06-09 16:05:59 -0700

    initial commit

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+class TaskController extends Controller
+{
+    public function show($id)
+    {
+
+    }
+
+    public function edit($id)
+    {
+        
+    }
+}
