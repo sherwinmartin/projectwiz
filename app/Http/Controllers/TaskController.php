247096c5442ba7e66761463ae4e3ab324b69f475
sherwin m <sherwinm@gmail.com>
2016-06-10 15:33:15 -0700

    finished crud

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -6,15 +6,133 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 
+use App\Models\Milestone;
+use App\Models\Task;
+
+use Redirect;
+
 class TaskController extends Controller
 {
+    /**
+     * Create array for completion_status values.
+     * @return mixed
+     */
+    private function percentages()
+    {
+        for($i=100; $i>=0; $i-=10)
+        {
+            $result[$i] = $i.'%';
+        }
+
+        return $result;
+    }
+
+    /**
+     * Display create task form.
+     * @param Request $request
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\Http\RedirectResponse|\Illuminate\View\View
+     */
+    public function create(Request $request)
+    {
+        $milestone = Milestone::find($request['milestone_id']);
+
+        if (!$milestone)
+        {
+            return back()->with('warning', 'Milestone not found.');
+        }
+
+        $data = [
+            'page_title'        => 'Create New Task',
+            'navi_group'        => 'tasks',
+            'navi_submenu'      => 'create',
+            'milestone'         => $milestone,
+            'percentages'       => $this->percentages(),
+            'predecessor_tasks' => Task::where('milestone_id', $milestone->id)->lists('task_name', 'id')->toArray(),
+        ];
+
+        return view('tasks.create', $data);
+    }
+
+    /**
+     * Create new task record.
+     * @param Requests\TaskRequest $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function store(Requests\TaskRequest $request)
+    {
+        if (Task::storeRecord($request))
+        {
+            return redirect::to('/milestones/'.$request['milestone_id'])->with('success', 'Task created.');
+        }
+
+        return back()->with('error', 'Task not created.');
+    }
+
     public function show($id)
     {
 
     }
 
+    /**
+     * Edit task record.
+     * @param $id
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\Http\RedirectResponse|\Illuminate\View\View
+     */
     public function edit($id)
     {
-        
+        $task = Task::find($id);
+
+        if (!$task)
+        {
+            return back()->with('warning', 'Task not found.');
+        }
+
+        $data = [
+            'page_title'        => 'Edit Task',
+            'navi_group'        => 'tasks',
+            'navi_submenu'      => 'edit',
+            'task'              => $task,
+            'milestones'        => Milestone::where('project_id', $task->milestone->project->id)->orderBy('milestone_name')->lists('milestone_name', 'id'),
+            'predecessor_tasks' => Task::where('milestone_id', $task->milestone_id)->where('id', '!=', $id)->lists('task_name', 'id')->toArray(),
+            'has_task_user'     => Task::hasTaskUser($id),
+            'has_predecessor_task'  => Task::hasPredecessorTask($id),
+            'percentages'       => $this->percentages()
+        ];
+
+        #dd($this->percentages());
+
+        return view('tasks.edit', $data);
+    }
+
+    /**
+     * Update task record.
+     * @param Requests\TaskRequest $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function update(Requests\TaskRequest $request)
+    {
+        if (Task::updateRecord($request))
+        {
+            return back()->with('success', 'Task updated.');
+        }
+
+        return back()->withInput()->with('error', 'Task not updated.');
+    }
+
+    /**
+     * Delete task record.
+     * @param $id
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function destroy($id)
+    {
+        $task = Task::find($id);
+
+        if ($task->delete())
+        {
+            return redirect::to('/milestones/'.$task->milestone_id)->with('success', 'Task deleted.');
+        }
+
+        return back()->with('error', 'Task not deleted.');
     }
 }
--- app/Models/Task.php
+++ app/Models/Task.php
@@ -7,4 +7,97 @@ use Illuminate\Database\Eloquent\Model;
 class Task extends Model
 {
     protected $table = 'tasks';
+
+    protected $fillable = [
+        'milestone_id',
+        'task_name',
+        'task_description',
+        'start_date',
+        'due_date',
+        'completion_status',
+        'notes',
+        'predecessor_task_id'
+    ];
+
+    /**
+     * Relationship to tasks table.
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function milestone()
+    {
+        return $this->belongsTo('App\Models\Milestone');
+    }
+
+    /**
+     * Relationship to task_users table.
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function taskUser()
+    {
+        return $this->hasMany('App\Models\TaskUser');
+    }
+
+    /**
+     * Check if user is assigned to this task.
+     * @param $task_id
+     * @return bool
+     */
+    public static function hasTaskUser($task_id)
+    {
+        if (TaskUser::where('task_id', $task_id)->first())
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Check if predecessor task exists.
+     * @param $task_id
+     * @return bool
+     */
+    public static function hasPredecessorTask($task_id)
+    {
+        if (Task::where('predecessor_task_id', $task_id)->first())
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Store new record for task.
+     * @param $request
+     * @return bool
+     */
+    public static function storeRecord($request)
+    {
+        $task = new Task;
+
+        if ($task::create($request->all()))
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Update task record.
+     * @param $request
+     * @return bool
+     */
+    public static function updateRecord($request)
+    {
+        $task = Task::find($request['id']);
+
+        if ($task->update($request->all()))
+        {
+            return true;
+        }
+
+        return false;
+    }
 }
