2c1cd6dd02c55894b065a5e0b0a4c39c3e5ce5d6
sherwin m <sherwinm@gmail.com>
2016-06-15 14:33:13 -0700

    added getAvailableUser($task_id) and getAssignedUser($task_id)

--- app/Models/TaskUser.php
+++ app/Models/TaskUser.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+use App\User;
+
 class TaskUser extends Model
 {
     protected $table = 'task_user';
@@ -24,5 +26,35 @@ class TaskUser extends Model
     public function user()
     {
         return $this->belongsTo('App\User');
+    }
+
+    /**
+     * Get users that have not yet been assigned to the task.
+     * @param $task_id
+     * @return mixed
+     */
+    public static function getAvailableUser($task_id)
+    {
+        $result = User::select('users.id', 'users.first_name')
+            ->whereNotExists(function ($query) use ($task_id)
+            {
+                $query->select('task_user.id')
+                    ->from('task_user')
+                    ->where('task_user.user_id', 'users.id')
+                    ->where('task_user.task_id', $task_id);
+            })->get();
+
+        return $result;
+    }
+
+    /**
+     * Get users assigned to task.
+     * @param $task_id
+     * @return mixed
+     */
+    public static function getAssignedUser($task_id)
+    {
+        return TaskUser::where('task_id', $task_id)
+            ->get();
     }
 }
