55e081542e913728dcac165b50c5608a9c8f4776
sherwin m <sherwinm@gmail.com>
2019-12-11 20:02:22 -0800

    initial commit

---
+++ resources/assets/js/bootstrap.js
@@ -0,0 +1,14 @@
+window._ = require('lodash');
+
+try
+{
+    window.Popper = require('popper.js').default;
+    window.$ = window.jQuery = require('jquery');
+
+    require('jquery-ui-dist/jquery-ui.min');
+
+} catch (e) {}
+
+window.axios = require('axios');
+
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
