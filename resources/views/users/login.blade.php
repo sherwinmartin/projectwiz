c411d352d94f47fd93891958b872aa01293c6945
sherwin m <sherwinm@gmail.com>
2019-12-12 07:43:19 -0800

    replaced {!! !!}} with {{}} and use route instead of action

--- resources/views/users/login.blade.php
+++ resources/views/users/login.blade.php
@@ -6,19 +6,19 @@
             <div class="page-header">
                 <h1>{{ $page_title }}</h1>
             </div>
-            {!! Form::open(['method' => 'POST', 'action' => 'UserController@authenticate']) !!}
+            {{ Form::open(['method' => 'POST', 'route' => 'authenticate']) }}
             <div class="form-group">
-                {!! Form::label('username', '*Username:') !!}
-                {!! Form::text('username', '', ['class' => 'form-control', 'id' => 'username', 'required', 'autofocus']) !!}
+                {{ Form::label('username', '*Username:') }}
+                {{ Form::text('username', '', ['class' => 'form-control', 'id' => 'username', 'required', 'autofocus']) }}
             </div>
 
             <div class="form-group">
-                {!! Form::label('password', '*Password:') !!}
-                {!! Form::password('password', ['class' => 'form-control', 'id' => 'password', 'required']) !!}
+                {{ Form::label('password', '*Password:') }}
+                {{ Form::password('password', ['class' => 'form-control', 'id' => 'password', 'required']) }}
             </div>
 
-            {!! Form::submit('Log In', ['class' => 'btn btn-primary']) !!} {!! link_to_action('Auth\PasswordController@getEmail', 'Password Help') !!}
-            {!! Form::close() !!}
+            {{ Form::submit('Log In', ['class' => 'btn btn-primary']) }} {{ link_to_action('Auth\PasswordController@getEmail', 'Password Help') }}
+            {{ Form::close() }}
         </div>
     </div>
 @endsection
