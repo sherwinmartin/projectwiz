e0a36571fb6564ac31a4d6ed7accf4a6bfdb28cb
sherwin m <sherwinm@gmail.com>
2019-12-12 08:42:36 -0800

    adjust css

--- resources/views/holidays/index.blade.php
+++ resources/views/holidays/index.blade.php
@@ -1,38 +1,50 @@
-@extends('layouts.default')
+@extends('layouts.app')
 
 @section('content')
     <ul class="breadcrumb">
-        <li>{{ link_to_action('DashboardController@index', 'Home') }}</li>
-        <li class="active">{{ $page_title }}</li>
+        <li class="breadcrumb-item">
+            {{ link_to_route('dashboard', 'Home') }}
+        </li>
+        <li class="breadcrumb-item active">{{ $page_title }}</li>
     </ul>
 
     <div class="page-header">
         <h2>{{ $page_title }}</h2>
     </div>
 
-    @if (!$holidays->isEmpty())
-        <table class="table table-striped">
-            <thead>
-                <tr>
-                    <th>Holiday Name</th>
-                    <th>Date</th>
-                    <th>&nbsp;</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($holidays as $holiday)
-                    <tr>
-                        <td>{{ $holiday->holiday_name }}</td>
-                        <td>{{ $holiday->holiday_date }}</td>
-                        <td class="text-right">{{ link_to_action('HolidayController@edit', 'edit', ['id' => $holiday->id], ['class' => 'btn btn-default']) }}</td>
-                    </tr>
-                @endforeach
-            </tbody>
-        </table>
-    @else
-        <div class="alert alert-info">
-            <p>Holiday not found.</p>
+    <section id="holidays" class="mt-3">
+        <div class="card">
+            <div class="card-body">
+                @if (!$holidays->isEmpty())
+                    <table class="table table-striped">
+                        <thead>
+                            <tr>
+                                <th>Holiday Name</th>
+                                <th>Date</th>
+                                <th>&nbsp;</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($holidays as $holiday)
+                                <tr>
+                                    <td>{{ $holiday->name }}</td>
+                                    <td>{{ $holiday->holiday_date }}</td>
+                                    <td class="text-right">
+                                        <a href="{{ route('holidays.edit', ['holiday' => $holiday->id]) }}" class="btn btn-outline-secondary">
+                                            <i class="fas fa-wrench"></i>
+                                        </a>
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                @else
+                    <div class="alert alert-info">
+                        <p>Holiday not found.</p>
+                    </div>
+                @endif
+            </div>
         </div>
-    @endif
+    </section>
 
 @endsection
