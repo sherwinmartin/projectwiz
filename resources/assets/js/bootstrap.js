1be12f4fb698f6687724e655ece16c5ce5d1dc84
sherwin m <sherwinm@gmail.com>
2019-12-11 20:10:21 -0800

    added code to include jquery and bootstrap js... oops ;)

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -3,7 +3,9 @@ window._ = require('lodash');
 try
 {
     window.Popper = require('popper.js').default;
-    window.$ = window.jQuery = require('jquery');
+    window.$ = window.jQuery = require('jquery/dist/jquery');
+
+    require('bootstrap');
 
     require('jquery-ui-dist/jquery-ui.min');
 
@@ -12,3 +14,11 @@ try
 window.axios = require('axios');
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+let token = document.head.querySelector('meta[name="csrf-token"]');
+
+if (token) {
+    window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
+} else {
+    console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
+}
