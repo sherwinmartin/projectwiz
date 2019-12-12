604fee3484e835faae6497673a3605589c3c9aa3
sherwin m <sherwinm@gmail.com>
2019-12-12 08:23:57 -0800

    fixed styling

--- resources/views/holidays/create.blade.php
+++ resources/views/holidays/create.blade.php
@@ -15,26 +15,41 @@
         <h2>{{ $page_title }}</h2>
     </div>
 
-    {{ Form::open(['method' => 'POST', 'action' => 'HolidayController@store']) }}
-        <div class="row">
-            <div class="col-md-8 col-sm-8">
-                <div class="form-group">
-                    {{ Form::label('holiday_name', '*Holiday Name:') }}
-                    {{ Form::text('holiday_name', NULL, ['class' => 'form-control']) }}
-                </div>
-            </div>
-            <div class="col-md-4 col-sm-4">
-                {{ Form::label('holiday_date', '*Holiday Date:') }}
-                <div class="input-group">
-                    {{ Form::text('holiday_date', date('Y-m-d'), ['class' => 'form-control', 'readonly' => 'readonly']) }}
-                    <label for="holiday_date" class="input-group-addon btn">
-                        <i class="fa fa-calendar"></i>
-                    </label>
-                </div>
+    <section id="create-form" class="mt-3">
+        <div class="card">
+            <div class="card-body">
+                {{ Form::open(['method' => 'POST', 'action' => 'HolidayController@store']) }}
+                    <div class="row">
+                        <div class="col-md-8 col-sm-8">
+                            <div class="form-group">
+                                {{ Form::label('holiday_name', '*Holiday Name:') }}
+                                {{ Form::text('holiday_name', NULL, ['class' => 'form-control']) }}
+                            </div>
+                        </div>
+                        <div class="col-md-3 col-sm-4">
+                            {{ Form::label('holiday_date', '*Holiday Date:') }}
+                            <div class="input-group">
+                                {{ Form::text('holiday_date', date('Y-m-d'), ['class' => 'form-control', 'readonly' => 'readonly']) }}
+                                <div class="input-group-append">
+                                    <label for="holiday_date" class="input-group-text">
+                                        <i class="fas fa-calendar"></i>
+                                    </label>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <button class="btn btn-primary" type="submit">
+                            Submit
+                        </button>
+                    </div>
+                {{ Form::close() }}
             </div>
         </div>
-        {{ Form::submit('Submit', ['class' => 'btn btn-primary']) }}
-    {{ Form::close() }}
+    </section>
+
 @endsection
 
 @section('custom_js_footer')
